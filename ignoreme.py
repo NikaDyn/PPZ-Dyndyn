a4d5b6c138eb833d54decbf21f9a46ca18d2eac4
NikaDyn <mariiaveronika.i.dyndyn@ukd.edu.ua>
2025-03-17 14:42:13 +0200

    Add help line

--- ignoreme.py
+++ ignoreme.py
@@ -1,1 +1,7 @@
 print("I’m gonna be ignored file")
+
+# git add [file]
+# git commit -m “[descriptive message]”
+# git push
+# git branch [branch-name] - create a new branch at the current commit
+# git checkout - switch to another branch and check it out into your working directory
