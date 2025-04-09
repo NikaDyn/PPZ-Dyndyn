fca6125dc0e535c99ceebf700b33bfb56d8011a1
NikaDyn <mariiaveronika.i.dyndyn@ukd.edu.ua>
2025-04-09 12:21:01 +0300

    Add task 10.2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from polls.views import question_list_view
-from .views import register_view
+from polls.views import *
 
 app_name = 'polls'
 urlpatterns = [
     path('', question_list_view, name='question_list'),
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 from .models import Question
-from django.contrib.auth import login
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserLoginForm
 
 
 def question_list_view(request):
@@ -21,3 +21,24 @@ def register_view(request):
         form = RegistrationForm()
 
     return render(request, 'polls/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('polls:question_list')
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'polls/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
